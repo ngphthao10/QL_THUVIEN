43781e556ecbae3297fa104579dd0d312120870f
aklc9210 <152038021+aklc9210@users.noreply.github.com>
2024-05-28 12:20:17 +0700

    Add files via upload

---
+++ src/main/java/poly/recaptcha/Constraint.java
@@ -0,0 +1,9 @@
+package poly.recaptcha;
+
+public class Constraint {
+	
+	public static final String SITE_KEY = "6LfYDOQpAAAAAFFMDTMGdXzPQedIqR2c5y_ieTUw";
+	public static final String SECRET_KEY = "6LfYDOQpAAAAACObN5HtM1cCM2rUrHRJvjtV85wj";
+	
+	
+}
