59449ca7fd7b50ea5fbd7cec85bedf68d5d410e1
NgocYenDg <163749076+NgocYenDg@users.noreply.github.com>
2024-05-29 22:15:22 +0700

    Add files via upload

---
+++ src/main/java/poly/dto/DocGiaDTO.java
@@ -0,0 +1,36 @@
+package ptithcm.dto;
+
+import ptithcm.entity.DocGia;
+import ptithcm.entity.NguoiDung;
+
+public class DocGiaDTO {
+	private DocGia docgia;
+	private NguoiDung nguoidung;
+	
+	public DocGiaDTO() {
+		super();
+	}
+
+	public DocGiaDTO(DocGia docgia, NguoiDung nguoidung) {
+		super();
+		this.docgia = docgia;
+		this.nguoidung = nguoidung;
+	}
+
+	public DocGia getDocgia() {
+		return docgia;
+	}
+
+	public void setDocgia(DocGia docgia) {
+		this.docgia = docgia;
+	}
+
+	public NguoiDung getNguoidung() {
+		return nguoidung;
+	}
+
+	public void setNguoidung(NguoiDung nguoidung) {
+		this.nguoidung = nguoidung;
+	}
+	
+}
